3dfa66f6a8b3a5eb08286fb4794117c6901b25a4
Gaurisha Maheshwari <igaurisha@gmail.com>
2022-05-06 21:25:14 +0530

    Add

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-const express= require('express')
-const app=express()
-const mongoose=require('mongoose')
-const PORT=5000
-const MONGOURI=require('./keys');
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const PORT = 3000;
+const { MONGOURI } = require("./keys");
 
-mongoose.connect(MONGOURI,{useNewUrlParser:true,})
-mongoose.connection.on('connected',()=>{
-    console.log("connected to mongo")
-})
-mongoose.connection.on('connected',(err)=>{
-    console.log("error in mongo")
-})
-app.listen(PORT,()=>{
-    console.log("server is running on",PORT)
-})
+require('./models/user')
+
+app.use(require('./routes/auth'))
+mongoose
+  .connect(MONGOURI)
+  .then(() => console.log("connected to mongo"))
+  .catch((err) => console.log("error in mongo"));
+
+app.listen(PORT, () => {
+  console.log("server is running on", PORT);
+});
+
+
--- keys.js
+++ keys.js
@@ -1,3 +1,5 @@
-module.exports={
-   MONGOURI:"mongodb+srv://gaurisha:WhVLhwZyUU9W2qBQ@cluster0.wzre5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-}
+module.exports = {
+   MONGOURI:
+     "mongodb+srv://gaurisha:WhVLhwZyUU9W2qBQ@cluster0.wzre5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+ };
+ 
